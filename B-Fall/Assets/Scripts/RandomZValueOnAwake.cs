d7b5a7b43f166f621bd74faa0983ba7c88bb0f1c
Ilia <52408039+IliaBahmutov@users.noreply.github.com>
2020-04-10 15:12:22 +0100

    Main Menu Stuff

---
+++ B-Fall/Assets/Scripts/RandomZValueOnAwake.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomZValueOnAwake : MonoBehaviour
+{
+	private void Awake()
+	{
+		transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, Random.Range(0,90f));
+	}
+}
