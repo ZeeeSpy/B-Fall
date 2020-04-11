830badb13dbb860c39a8cbc09c10561f65eab7e5
Ilia <52408039+IliaBahmutov@users.noreply.github.com>
2020-04-11 18:55:16 +0100

    Update PointTrigger.cs

--- B-Fall/Assets/Scripts/PointTrigger.cs
+++ B-Fall/Assets/Scripts/PointTrigger.cs
@@ -4,11 +4,32 @@ using UnityEngine;
 
 public class PointTrigger : MonoBehaviour
 {
+
+	private bool Activated = false;
+	private bool Called = false;
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.gameObject.CompareTag("Player"))
+		{
+			Activated = true;
+		}
+	}
 	private void OnTriggerExit2D(Collider2D collision)
 	{
 		if (collision.gameObject.CompareTag("Player"))
 		{
-			Points.Instance.IncreaseScore();
+			if (Activated && !Called)
+			{
+				Called = true;
+				Points.Instance.IncreaseScore();
+			}
 		}
+	}
+
+	public void ResetPoint()
+	{
+		Activated = false;
+		Called = false;
 	}
 }
